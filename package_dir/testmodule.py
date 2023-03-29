c883a6193e3b41149bdbe6b44b0e4a3989a4daed
Vic <victor@teelucksingh.com>
2023-03-28 20:20:21 -0400

    Add package & module files

---
+++ package_dir/testmodule.py
@@ -0,0 +1,6 @@
+def myfunction():
+    '''Test function in test module'''
+    print("This is a test.")
+
+if __name__ == '__main__':
+    myfunction()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='Test',
+      version ='0.1',
+      description='Test packages and modules',
+      packages=['package_dir'])
